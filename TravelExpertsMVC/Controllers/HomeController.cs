b514789582c49312fdeb578998a819d837c176d6
justinmolnar <1justin.molnar@gmail.com>
2022-02-10 11:29:00 -0700

    Added basic front and backend skeleton
    Moved DB files to new project
    +Added blank website pages

--- TravelExpertsMVC/Controllers/HomeController.cs
+++ TravelExpertsMVC/Controllers/HomeController.cs
@@ -1,37 +1,135 @@
-﻿using Microsoft.AspNetCore.Mvc;
-using Microsoft.Extensions.Logging;
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
 using System;
 using System.Collections.Generic;
-using System.Diagnostics;
 using System.Linq;
 using System.Threading.Tasks;
-using TravelExpertsMVC.Models;
 
-namespace TravelExpertsMVC.Controllers
+namespace TravelExpertsMVC
 {
-	public class HomeController : Controller
-	{
-		private readonly ILogger<HomeController> _logger;
+    public class HomeController : Controller
+    {
+        // GET: HomeController
+        public ActionResult Index()
+        {
+            return View();
+        }
 
-		public HomeController(ILogger<HomeController> logger)
-		{
-			_logger = logger;
-		}
+        // GET: HomeController/Details/5
+        public ActionResult Details(int id)
+        {
+            return View();
+        }
 
-		public IActionResult Index()
-		{
-			return View();
-		}
+        // GET: HomeController/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
 
-		public IActionResult Privacy()
-		{
-			return View();
-		}
+        // GET: HomeController/Contact
+        public ActionResult Contact()
+        {
+            return View();
+        }
 
-		[ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
-		public IActionResult Error()
-		{
-			return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
-		}
-	}
+        // GET: HomeController/Packages
+        public ActionResult Packages()
+        {
+            return View();
+        }
+
+        // GET: HomeController/Login
+        public ActionResult Login()
+        {
+            return View();
+        }
+
+        // GET: HomeController/CustomerHome
+        public ActionResult CustomerHome()
+        {
+            return View();
+        }
+
+        // GET: HomeController/Order
+        public ActionResult Order()
+        {
+            return View();
+        }
+
+        // GET: HomeController/OrderPlaced
+        public ActionResult OrderPlaced()
+        {
+            return View();
+        }
+
+        // GET: HomeController/Register
+        public ActionResult Register()
+        {
+            return View();
+        }
+
+        // GET: HomeController/ThankYou
+        public ActionResult ThankYou()
+        {
+            return View();
+        }
+
+        // POST: HomeController/Create
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create(IFormCollection collection)
+        {
+            try
+            {
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: HomeController/Edit/5
+        public ActionResult Edit(int id)
+        {
+            return View();
+        }
+
+        // POST: HomeController/Edit/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit(int id, IFormCollection collection)
+        {
+            try
+            {
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: HomeController/Delete/5
+        public ActionResult Delete(int id)
+        {
+            return View();
+        }
+
+        // POST: HomeController/Delete/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Delete(int id, IFormCollection collection)
+        {
+            try
+            {
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+    }
 }
