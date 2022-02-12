80ead38c66b92893ce3f7a16866299266048e883
justinmolnar <1justin.molnar@gmail.com>
2022-02-12 16:12:15 -0700

    Added contact list to contacts page
    contact list pulling from db

--- TravelExpertsMVC/Controllers/HomeController.cs
+++ TravelExpertsMVC/Controllers/HomeController.cs
@@ -34,7 +34,54 @@ namespace TravelExpertsMVC
         // GET: HomeController/Contact
         public ActionResult Contact()
         {
-            return View();
+            List<Agent> agents = null;
+            try
+            {
+                agents = AgentManager.GetAgents();
+            }
+            catch (Exception)
+            {
+                TempData["Message"] = "Database connection problem. Try again later.";
+                TempData["IsError"] = true;
+            }
+
+            List<Agency> agencies = null;
+            try
+            {
+                agencies = AgentManager.GetAgencies();
+            }
+            catch (Exception)
+            {
+                TempData["Message"] = "Database connection problem. Try again later.";
+                TempData["IsError"] = true;
+            }
+            return View(agents, agencies);
+        }
+
+        private ActionResult View(List<Agent> agents, List<Agency> agencies)
+        {
+           // List<Agent> agents = null;
+            try
+            {
+                agents = AgentManager.GetAgents();
+            }
+            catch (Exception)
+            {
+                TempData["Message"] = "Database connection problem. Try again later.";
+                TempData["IsError"] = true;
+            }
+
+          //  List<Agency> agencies = null;
+            try
+            {
+                agencies = AgentManager.GetAgencies();
+            }
+            catch (Exception)
+            {
+                TempData["Message"] = "Database connection problem. Try again later.";
+                TempData["IsError"] = true;
+            }
+            return View(agents, agencies);
         }
 
         // GET: HomeController/Packages
