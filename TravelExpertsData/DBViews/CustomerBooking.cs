2f38539cfbc27fe8e4988876d303ca4c29061de3
Filip <fflorek.7@gmail.com>
2022-02-14 12:42:47 -0700

    display packages of single user

---
+++ TravelExpertsData/DBViews/CustomerBooking.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TravelExpertsData.DBViews
+{
+    public class CustomerBooking
+    {
+        public int BookingId { get; set; }
+        public string BookingNo { get; set; }
+        public DateTime? BookingDate { get; set; }
+        public DateTime? TripStart { get; set; }
+        public DateTime? TripEnd { get; set; }
+        public string Description { get; set; }
+        public List<string> ProductsInBooking { get; set; }
+        public decimal TotalPrice { get; set; }
+    }
+}
---
+++ TravelExpertsMVC/Controllers/CustomerBookingController.cs
@@ -0,0 +1,91 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using TravelExpertsData.DBViews;
+using TravelExpertsData.Managers;
+
+namespace TravelExpertsMVC.Controllers
+{
+    public class CustomerBookingController : Controller
+    {
+        // GET: CustomerBookingController
+        public ActionResult Index()
+        {
+            int customerID = 143;
+            List<CustomerBooking> bookings = BookingManager.GetCustomerBookings(customerID);
+            return View(bookings);
+        }
+
+        // GET: CustomerBookingController/Details/5
+        public ActionResult Details(int id)
+        {
+            return View();
+        }
+
+        // GET: CustomerBookingController/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: CustomerBookingController/Create
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create(IFormCollection collection)
+        {
+            try
+            {
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: CustomerBookingController/Edit/5
+        public ActionResult Edit(int id)
+        {
+            return View();
+        }
+
+        // POST: CustomerBookingController/Edit/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit(int id, IFormCollection collection)
+        {
+            try
+            {
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: CustomerBookingController/Delete/5
+        public ActionResult Delete(int id)
+        {
+            return View();
+        }
+
+        // POST: CustomerBookingController/Delete/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Delete(int id, IFormCollection collection)
+        {
+            try
+            {
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+    }
+}
